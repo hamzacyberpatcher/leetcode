5058af67166f5ea08f849c968d21303bd1d4b6e8
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-02-09 09:54:16 +0500

    added rotate image q48

---
+++ q48_RotateImage.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int size = matrix.size();
+
+        for(int i = 0; i < size; i++)
+            for(int j = i + 1; j < size; j++)
+                swap(matrix[i][j], matrix[j][i]);
+        
+        for(int j = 0; j < size/2; j++)
+            for(int i = 0; i < size; i++)
+                swap(matrix[i][j], matrix[i][size - j - 1]);
+    }
+};
