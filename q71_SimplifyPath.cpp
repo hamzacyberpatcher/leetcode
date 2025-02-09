53f305fa6cded793131aff6060e5a7f265e674b6
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-02-09 10:03:00 +0500

    added simplify path q71

---
+++ q71_SimplifyPath.cpp
@@ -0,0 +1,15 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        path = path.replace("//","/")
+        directories = path.split("/")
+        path_list = []
+        for directory in directories:
+            if (directory != ".." and directory != "" and directory != "."):
+                path_list.append(directory)
+            elif directory == ".." and len(path_list) != 0:
+                path_list.pop()
+        path = ""
+        for folder in path_list:
+            path += f"/{folder}"
+        if len(path_list) == 0: path = "/"
+        return path
