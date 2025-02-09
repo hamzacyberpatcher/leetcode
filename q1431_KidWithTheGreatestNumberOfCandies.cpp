6bd4148508a624eac3c909ddff4998225a0bc4d7
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-02-09 10:17:05 +0500

    added kids with the greatest number of candies q1431

---
+++ q1431_KidWithTheGreatestNumberOfCandies.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        vector<bool> extra;
+        int max = *max_element(candies.begin(), candies.end());
+        
+        for (int candy : candies)
+        {
+            if (candy + extraCandies >= max) extra.push_back(true);
+            else extra.push_back(false);
+        }
+
+        return extra;
+    }
+};
