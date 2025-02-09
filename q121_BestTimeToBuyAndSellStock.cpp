09dd4e425026bafdb9c24cf686140e530027ba11
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-02-09 09:44:14 +0500

    added best time to buy and sell stock

---
+++ q121_BestTimeToBuyAndSellStock.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int size = prices.size();
+        int l = 0;
+        int r = 1;
+        int maxP = 0;
+        int profit;
+
+        while (r < size)
+        {
+            if (prices[r] > prices[l])
+            {
+                profit = prices[r] - prices[l];
+                if (profit > maxP) maxP = profit;
+            }
+            else l = r;
+            r++;
+        }
+
+        return maxP;
+    }
+};
