3ab31ad206bc583dd80ae2db96ca27e1c239830f
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-02-09 09:50:38 +0500

    added container with most water q11

---
+++ q11_ContainerWithMostWater.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l = 0;
+        int r = height.size() - 1;
+        int area;
+        int maxA = 0;
+        int minH;
+
+        while(l < r)
+        {
+            minH = min(height[l], height[r]);
+            area = minH * (r - l);
+
+            maxA = max(maxA, area);
+
+            if (height[l] < height[r]) l++;
+            else r--;
+            
+        }
+
+        
+
+        return maxA;
+        
+    }
+};
