c1cfa523d5e9708d271d49300af4ee1dc8a4078a
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-02-09 10:12:16 +0500

    added move zeroes q283

---
+++ q283_MoveZeroes.cpp
@@ -0,0 +1,41 @@
+/*
+void printvec(const vector<int>& vec)
+{
+    for(int n : vec) cout << n << " ";
+    cout << endl;
+}
+*/
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int size = nums.size();
+        int l = 0;
+        int r = 1;
+
+        if (size == 1) return;
+
+        while (l < size - 1)
+        {
+            if (nums[l] == 0)
+            {
+                while(nums[r] == 0 && r < size - 1) r++;
+                /*
+                cout << "Before swap: " << endl;
+                cout << "l: " << l << endl << "r: " << r << endl;
+                printvec(nums);
+                */
+                swap(nums[l], nums[r]);
+                /*
+                cout << "After swap: " << endl;
+                cout << "l: " << l << endl << "r: " << r << endl;
+                printvec(nums);
+                cout << endl;
+                */
+            }
+            l++;
+            r = l + 1;
+        }
+        
+    }
+};
