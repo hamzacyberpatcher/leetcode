91f715f10ef5ba6686fb5e95807fc6480d403c24
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-02-09 09:55:02 +0500

    added set matrix zeroes q73

---
+++ q73_SetMatrixZeroes.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        vector<int> cols;
+        vector<int> rows;
+
+        for(int i = 0; i < matrix.size(); i++)
+        {
+            for(int j = 0; j < matrix[i].size(); j++)
+            {
+                if (matrix[i][j] == 0)
+                {
+                    rows.push_back(i);
+                    cols.push_back(j);
+                }
+            }
+        }
+
+        for(int i = 0; i < rows.size(); i++)
+        {
+            int row = rows[i];
+
+            for(int j = 0; j < matrix[row].size(); j++)
+                matrix[row][j] = 0;
+        }
+
+        for(int i = 0; i < cols.size(); i++)
+        {
+            int col = cols[i];
+
+            for(int j = 0; j < matrix.size(); j++)
+                matrix[j][col] = 0;
+        }
+    }
+};
