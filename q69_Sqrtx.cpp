6899b2ece102e6f372a2ab1a961eb18468b50826
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-02-09 10:07:27 +0500

    added sqrt x q69

---
+++ q69_Sqrtx.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+
+        if (x == 0) return 0;
+        if (x < 1) return 1;
+        if (x == 1) return 1;
+        
+        int left = 0;
+        int right = x / 2;
+
+        int mid = left + (right - left) / 2;
+
+        while(left <= right)
+        {
+            mid = left + (right - left) / 2;
+
+            long long squared = static_cast<long long>(mid) * mid;
+
+            if (squared > x)
+                right = mid - 1;
+            else if (squared < x)
+                left = mid + 1;
+            else
+                return mid;
+        }
+
+        return right;
+    }
+};
