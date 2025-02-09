3ea6453163fbc2ad4b31c8e5439caef36cfa26d6
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-02-09 10:06:11 +0500

    added plus one q66

---
+++ q66_PlusOne.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        bool carry = true;
+
+        for(int i = digits.size() - 1; i >= 0 && carry; i--)
+        {
+            int digit = digits[i] + 1;
+
+            if (digit > 9)
+                digits[i] = 0;
+            else
+            {
+                digits[i] = digit;
+                carry = false;
+            }
+        }
+
+        if (carry)
+        {
+            digits.insert(digits.begin(), 1);
+        }
+
+        return digits;
+    }
+};
