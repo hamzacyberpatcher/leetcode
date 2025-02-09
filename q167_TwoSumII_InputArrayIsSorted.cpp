224c83da096a420b9505bc10550baf63cf445207
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-02-09 09:49:55 +0500

    added two sum II - input is sorted q167

---
+++ q167_TwoSumII_InputArrayIsSorted.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+     int sum;
+     int l = 0;
+     int r = numbers.size() - 1;
+     vector<int> sol(2);
+
+     while (l < r)
+     {
+        sum = numbers[l] + numbers[r];
+        if (sum == target)
+        {
+            sol[0] = l+1;
+            sol[1] = r+1;
+            return sol;
+        }
+        else if (sum <= target) l++;
+        else r--;
+     }
+     return sol;
+    }
+};
