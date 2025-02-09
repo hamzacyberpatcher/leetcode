b2efd2d53bf1574a8c1ed1cef726c060f0884d82
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-02-09 17:02:26 +0500

    added linked list cycle q141

---
+++ q141_LinkedListCycle.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+    vector<ListNode*> vec;
+public:
+    bool hasCycle(ListNode *head) {
+
+        if (head == NULL) return false;
+        
+        while(head->next != NULL)
+        {
+            auto it = find(vec.begin(), vec.end(), head);
+            if (it != vec.end())
+            {
+                return true;
+            }
+            else
+            {
+                vec.push_back(head);
+            }
+            head = head->next;
+        }
+
+        return false;
+
+    }
+};
