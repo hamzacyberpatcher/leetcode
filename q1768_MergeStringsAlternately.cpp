07aeef7805b2fb6ea63b10d1d058ae1270c893be
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-02-09 10:18:01 +0500

    added merge strings alternately q1768

---
+++ q1768_MergeStringsAlternately.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int size1 = word1.length();
+        int size2 = word2.length();
+
+        string result = "";
+
+        int i;
+
+        for (i = 0; i < min(size1, size2); i++)
+        {
+            result = result + word1[i] + word2[i];
+        }
+
+
+        if (size1 > size2) result += word1.substr(i, size1);
+        else if (size1 < size2) result += word2.substr(i, size2);
+
+
+        return result;
+    }
+};
