8f478ae014b8cddab5623b4a9494ce5b4b715788
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-02-09 10:14:53 +0500

    added is subsequence q392

---
+++ q392_IsSubsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int l = 0;
+        int r = 0;
+        int sub_size = s.length();
+        int string_size = t.length();
+
+        while(l < sub_size && r < string_size)
+        {
+            if (s[l] == t[r]) l++;
+            r++;
+        }
+
+        if (l == sub_size) return true;
+        return false;
+    }
+};
