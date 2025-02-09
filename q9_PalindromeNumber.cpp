bf1cab4a31fffeef9432425534572e5909bd9e21
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-02-09 10:05:30 +0500

    added palindrome number q9

---
+++ q9_PalindromeNumber.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0)
+            return false;
+        
+        unsigned int revNum = 0;
+
+        int temp = x;
+
+        while(temp > 0)
+        {
+            revNum *= 10;
+            revNum += temp % 10;
+            temp /= 10;
+        }
+
+        while (x > 0)
+        {
+            if (x % 10 != revNum % 10)
+                return false;
+            
+            x /= 10;
+            revNum /= 10;
+        }
+
+        return true;
+    }
+};
