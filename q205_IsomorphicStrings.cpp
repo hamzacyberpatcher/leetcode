a2a50eaa1efc0a8b50d9a322ca8b0b58373e7569
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-02-09 09:57:27 +0500

    added isomorphic strings q205

---
+++ q205_IsomorphicStrings.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, char> map;
+        
+        for(int i = 0; i < s.length(); i++)
+        {
+            if (map.find(s[i]) != map.end())
+            {
+                if (map[s[i]] != t[i])
+                    return false;
+            }
+            else map.insert({s[i], t[i]});
+        }
+
+        map.clear();
+
+        for(int i = 0; i < t.length(); i++)
+        {
+            if (map.find(t[i]) != map.end())
+            {
+                if (map[t[i]] != s[i])
+                    return false;
+            }
+            else map.insert({t[i], s[i]});
+        }
+
+        return true;
+        
+    }
+};
