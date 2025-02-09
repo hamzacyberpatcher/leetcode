44f4c6b79d345115521a7db7106a587644401a35
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-02-09 10:13:11 +0500

    added reverse vowels of a string q345

---
+++ q345_ReverseVowelsOfAString.cpp
@@ -0,0 +1,36 @@
+bool isVowel(char ch)
+{
+    switch(tolower(ch))
+    {
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+        return true;
+    default:
+        return false;
+    }
+}
+
+class Solution {
+public:
+    string reverseVowels(string s) {
+        int l = 0;
+        int r = s.size() - 1;
+
+        while(l < r)
+        {
+            while ( l < r && !isVowel(s[l])) l++;
+            while ( l < r && !isVowel(s[r])) r--;
+
+            swap(s[l], s[r]);
+
+            l++; r--;
+
+
+        }
+
+        return s;
+    }
+};
