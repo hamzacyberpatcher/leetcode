7da7053d43bf12dede1f30160f129296406c2052
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-02-09 10:04:49 +0500

    added evaluate reverse polish notation q150

---
+++ q150_EvaluateReversePolishNotation.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> s;
+
+        for(int i = 0; i < tokens.size(); i++)
+        {
+            string token = tokens[i];
+
+            if (token == "+" || token == "-" || token == "*" || token == "/")
+            {
+                int b = s.top();
+                s.pop();
+                int a = s.top();
+                s.pop();
+
+                int compute;
+
+                if (token == "+") {
+                    compute = a + b;
+                } else if (token == "-") {
+                    compute = a - b;
+                } else if (token == "*") {
+                    compute = a * b;
+                } else {
+                    compute = a / b;
+                }
+
+                s.push(compute);
+
+            } else {
+                s.push(stoi(token));
+            }
+        }
+
+        return s.top();
+    }
+};
