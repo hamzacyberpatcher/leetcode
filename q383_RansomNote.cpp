c5e8fe786ae91dbaf2cd25292438db7dd1700c24
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-02-09 09:56:35 +0500

    added ransom note q383

---
+++ q383_RansomNote.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> map;
+
+        for(char i = 'a'; i <= 'z'; i++)
+            map.insert({i, 0});
+        
+        for(const char &ch : magazine)
+            map[ch]++;
+
+        for(const char &ch : ransomNote)
+        {
+            if (map[ch] == 0) return false;
+            else map[ch]--;
+        }
+
+        return true;
+        
+    }
+};
