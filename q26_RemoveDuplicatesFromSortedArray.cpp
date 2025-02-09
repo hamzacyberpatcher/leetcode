eb37455404149483224d16c477c862f578a87d7b
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-02-09 09:37:59 +0500

    added remove duplicates from sorted array q26

---
+++ q26_RemoveDuplicatesFromSortedArray.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int unique;
+        vector<int>::iterator it = nums.begin();
+        for(int i = 0; i < nums.size(); i++)
+        {
+            unique = nums[i];
+            for(int j = i + 1; j < nums.size();)
+            {
+                if (nums[j] == unique) nums.erase(it + j);
+                else j++;
+            }
+        }
+        return nums.size();
+    }
+};
