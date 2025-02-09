68f63ad94dfc33c1dcb9e40a01adad20b0264724
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-02-09 10:01:21 +0500

    added contains duplicate q219

---
+++ q219_ContainsDuplicateII.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> map;
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if (map.find(nums[i]) != map.end())
+            {
+                if (abs(i - map[nums[i]]) <= k)
+                    return true;
+                else
+                    map[nums[i]] = i;
+            }
+            else
+            {
+                map.insert({nums[i], i});
+            }
+
+        }
+
+        return false;
+    }
+};
