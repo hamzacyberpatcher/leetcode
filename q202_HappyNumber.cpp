eaca7e78f3f3c8d4d4713d231d19da5b428e458c
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-02-09 10:00:03 +0500

    added happy number q202

---
+++ q202_HappyNumber.cpp
@@ -0,0 +1,36 @@
+class Solution {
+
+    int sumDigits(int n)
+    {
+        int sum = 0;
+
+        while(n > 0)
+        {
+            sum += (n % 10) * (n % 10);
+            n /= 10;
+        }
+
+        return sum;
+    }
+
+public:
+    bool isHappy(int n) {
+        unordered_map<int, bool> map;
+
+        int sum = sumDigits(n);
+
+        while(sum != 1)
+        {
+            if (map.find(sum) != map.end())     
+            {
+                return false;
+            }
+
+            map.insert({sum , true});
+
+            sum = sumDigits(sum);
+        }
+
+        return true;
+    }
+};
