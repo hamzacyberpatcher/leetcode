0e0de6d27eed42a5236d11c7df72c14eb4345900
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-02-09 09:41:46 +0500

    added rotate array q189

---
+++ q189_RotateArray.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int maj_elem;
+        int occurence;
+        int current_elem;
+        int max_occurence = 0;
+        int size = nums.size();
+        
+        for (int i = 0; i < nums.size(); i++)
+        {
+            current_elem = nums[i];
+            occurence = 0;
+            for(int j = 0; j < nums.size(); j++)
+            {
+                if (current_elem == maj_elem) break;
+                if (nums[j] == current_elem) occurence++;
+            }
+            if ((occurence >= size / 2) && (occurence > max_occurence)) 
+            {
+                maj_elem = current_elem;
+                max_occurence = occurence;
+            }
+        }
+
+        return maj_elem;
+
+    }
+};
