168e52c0f3e9d3ade9d11991ca949c5880a0e23c
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-02-09 09:37:01 +0500

    added remove element q27

---
+++ q27_RemoveElement.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        
+        vector<int>::iterator it = nums.begin();
+
+        for(int i = 0; i < nums.size();)
+        {
+            if (nums[i] == val) nums.erase(it + i);
+            else i++;
+        }
+
+        return nums.size();
+
+    }
+};
