48ec004cdccd10b4d8ef26e78457d0a76ecec734
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-02-09 09:40:21 +0500

    added remove duplicates from sorted array II q80

---
+++ q80_RemoveDuplicatesFromSortedArrayII.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+
+        int uni;
+        vector<int>::iterator it = nums.begin();
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            uni = nums[i];
+            for(int j = i + 2; j < nums.size();)
+            {
+                if (nums[j] == uni) nums.erase(it + j);
+                else j++;
+            }
+        }
+        return nums.size();
+    }
+};
