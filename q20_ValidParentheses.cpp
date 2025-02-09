4d680d1e8b7ea65ba5a402dd57bad29ded44f4f5
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-02-09 10:02:24 +0500

    added valid parenthese q20

---
+++ q20_ValidParentheses.cpp
@@ -0,0 +1,18 @@
+#include <stack>
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> balancer;
+
+        for(char ch : s)
+        {
+            if (ch == '(' || ch == '[' || ch == '{') balancer.push(ch);
+            else if (ch == ')' && !balancer.empty() && balancer.top() == '(') balancer.pop();
+            else if (ch == ']' && !balancer.empty() && balancer.top() == '[') balancer.pop();
+            else if (ch == '}' && !balancer.empty() && balancer.top() == '{') balancer.pop();
+            else return false;
+        }
+        return balancer.empty();
+    }
+};
