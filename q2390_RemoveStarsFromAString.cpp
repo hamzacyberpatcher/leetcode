ab1b3fbdd059b60ec358eac46fd4e8ca8311a6ab
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-02-09 10:18:52 +0500

    remove stars from a string q2390

---
+++ q2390_RemoveStarsFromAString.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+
+class Solution {
+public:
+    string removeStars(string s) {
+        vector<char> meow;
+
+        for (char ch : s)
+        {
+            (ch == '*') ? meow.pop_back() : meow.push_back(ch);
+        }
+        
+        string t;
+
+        for (char ch : meow) t += ch;
+
+        return t;
+    }
+};
