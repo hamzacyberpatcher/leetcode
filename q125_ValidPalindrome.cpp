5c1caf4f3e2e0fa1a7505c31fe36ed8e0b473999
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-02-09 09:48:56 +0500

    added valid palindrome q125

---
+++ q125_ValidPalindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int l = 0;
+        int r = s.length() - 1;
+
+        while (l < r)
+        {
+            while (l < r && !isalnum(s[l]) || (s[l] == ' ')) l++;
+            while (l < r && !isalnum(s[r]) || (s[r] == ' ')) r--;
+            if (l < r && tolower(s[l]) == tolower(s[r]))
+            {
+                l++;
+                r--;
+            }
+            else if (l < r) {
+                return false;
+            }
+
+        }
+        return true;
+    }
+};
