0041cc52c1654e27b5a54423eae3a0373a97bc2b
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-02-09 09:58:54 +0500

    added valid anagram q242

---
+++ q242_ValidAnagram.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+
+        unordered_map<char, int> map1;
+        unordered_map<char, int> map2;
+
+        for(int i = 0; i < s.length(); i++)
+        {
+            if (map1.find(s[i]) != map1.end()) map1[s[i]]++;
+            else map1.insert({s[i], 1});
+
+            if (map2.find(t[i]) != map2.end()) map2[t[i]]++;
+            else map2.insert({t[i], 1});
+        }
+
+        for(int i = 0; i < s.length(); i++)
+        {
+            
+            
+            if (map1[s[i]] != map2[s[i]])
+                return false;
+        }
+
+        return true;
+    }
+};
