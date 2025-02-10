1ed8c94d38952b1e32493afc338915d54320d931
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-02-10 19:26:29 +0500

    added remove duplicates from sorted list II q82

---
+++ q82_RemoveDuplicatesFromSortedListII.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        unordered_map<int, int> map;
+
+        ListNode * curr = head;
+
+        while(curr != nullptr)
+        {
+            if(map.find(curr->val) != map.end())
+            {
+                map[curr->val]++;
+            }
+            else
+            {
+                map.insert({curr->val, 1});
+            }
+            curr = curr->next;
+        }
+
+        ListNode dummy(0, head);
+        ListNode * prev = &dummy;
+        curr = head;
+
+        while(curr != nullptr)
+        {
+            if (map[curr->val] > 1)
+            {
+                ListNode* toDel = curr;
+                prev->next = curr->next;
+                curr = curr->next;
+                delete toDel;
+            }
+            else
+            {
+                prev = prev->next;
+                curr = curr->next;
+            }
+        }
+
+        return dummy.next;
+    }
+};
