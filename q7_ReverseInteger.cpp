837d2f6670b7b21de29f03eecf71747c2e2f9788
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-03-02 04:02:53 +0500

    added reverse integer q7

---
+++ q7_ReverseInteger.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long int reverse(int x) {
+        long int temp = 0;
+
+        bool isNeg = false;
+        if (x < 0 && x > INT_MIN)
+        {
+            isNeg = true;
+            x *= -1;
+        }
+
+        while(x > 0)
+        {
+            temp *= 10;
+            temp += x % 10;
+            x /= 10;
+        }
+
+        if (temp > INT_MAX || temp < INT_MIN)
+            return 0;
+
+        if (isNeg || x < 0) return -temp;
+
+        return temp;
+    }
+};
