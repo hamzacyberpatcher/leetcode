884d52fa1183d80ef42dcfb7f745617338ba3576
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-07-05 13:06:53 +0500

    added q14

---
+++ q14_LongestCommonPrefix.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.size() == 1)
+            return static_cast<string>(strs[0]);
+
+        string prefix = "";
+
+        int rows = strs.size();
+        bool mismatch = false;
+
+        for(int i = 0; i < strs[0].size() && !mismatch; i++)
+        {
+            for(int j = 0; j < rows - 1 && !mismatch; j++)
+                if (strs[j][i] != strs[j + 1][i])
+                    mismatch = true;
+            
+            if (!mismatch)
+            {
+                prefix += strs[0][i];
+            }
+        }
+
+        return prefix;
+    }
+};
