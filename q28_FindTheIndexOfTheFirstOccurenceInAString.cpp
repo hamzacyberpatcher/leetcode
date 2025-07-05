1d3bd9324a750c6a888d51e8e0eacb8b5ff7b421
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-07-05 13:30:56 +0500

    added q28

---
+++ q28_FindTheIndexOfTheFirstOccurenceInAString.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+
+        if (needle == haystack)
+            return 0;
+    
+
+     for(int i = 0; i < haystack.size(); i++)   
+     {
+        int matches = 0;
+        for(int j = i, k = 0; j < i + needle.size() && j < haystack.size(); j++, k++)
+            if (haystack[j] == needle[k])
+                matches++;
+            else
+                break;
+        
+        if (matches == needle.size())
+            return i;
+     }
+
+     return -1;
+    }
+};
