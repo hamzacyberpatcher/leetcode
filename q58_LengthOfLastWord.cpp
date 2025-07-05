6fc8005bcd89ec8b08394b179d284fced9e43ebc
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-07-05 12:59:05 +0500

    added q58

---
+++ q58_LengthOfLastWord.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string rtrim(string s)
+    {
+        int len = s.length();
+
+        if (len == 1 && s != " ") return s;
+        else if (len == 1 && s == " ") return "";
+
+        for(int i = len - 1; i >= 0; i--)
+        {
+            if (s[i] != ' ')
+                break;
+            else
+                s.erase(s.begin() + i);
+        }
+
+        return s;
+    }
+
+    int lengthOfLastWord(string s) {
+        s = rtrim(s);
+
+        int len = s.length();
+
+        int count = 0;
+
+        for(int i = len - 1; i >= 0; i--)
+        {
+            if (s[i] == ' ') break;
+            count++;
+        }
+        
+        return count;
+    }
+};
