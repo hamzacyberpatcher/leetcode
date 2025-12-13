5479c9c8a880726da39bc59de7f17a6e40b92001
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-12-13 11:45:02 +0500

    added q124

---
+++ q124_BinaryTreeMaximumPathSum.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int DFS(TreeNode* root, int& ans)
+    {
+        if (!root) return 0;
+
+        int lSum = DFS(root->left, ans);
+        int rSum = DFS(root->right, ans);
+
+        int maxSum = root->val + max(max(lSum, rSum), 0);
+
+        ans = max(max(ans, root->val + lSum + rSum), root->val + max(max(lSum, rSum), 0));
+
+        return maxSum;
+    }
+
+    int maxPathSum(TreeNode* root) {
+        int ans = root->val;
+
+        DFS(root, ans);
+
+        return ans;
+    }
+};
